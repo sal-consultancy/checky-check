a1ec9dee797f4598710391d83a963894c747bb33
Sjoerd Lubbers <sjoerd.lubbers@salconsultancy.nl>
2024-10-30 10:19:00 +0100

    chore: adding github reference to footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { FaGithub } from "react-icons/fa";
 
 const Footer = ({ copyright }) => {
   return (
     <footer className="footer">
       <div className="content has-text-centered">
         <p>{copyright}</p>
+        <p><a style={{ color: 'grey' }} target="_blank" href="https://github.com/sal-consultancy/checky-check"><FaGithub /> Github</a></p>
       </div>
     </footer>
   );
